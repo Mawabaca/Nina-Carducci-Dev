5c614ef6dcf94eb8460f4a76cea6576b4ad9d45b
Abderahmane <abderahmane.benomari7@gmail.com>
2024-12-26 19:38:19 +0100

    ff
    ff

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -154,13 +154,13 @@
         }
       });
 
-      index = (index - 1) 
-
       next =
-        imagesCollection[index] ||
+        imagesCollection[index - 1] ||
         imagesCollection[imagesCollection.length - 1];
+
       $(".lightboxImage").attr("src", $(next).attr("src"));
     },
+
     nextImage() {
       let activeImage = null;
       $("img.gallery-item").each(function() {
@@ -196,10 +196,8 @@
         }
       });
 
-      index = (index + 1) 
-
       next = 
-      imagesCollection[index] || imagesCollection[0];
+      imagesCollection[index + 1] || imagesCollection[0];
       $(".lightboxImage").attr("src", $(next).attr("src"));
     },
     createLightBox(gallery, lightboxId, navigation) {
@@ -247,7 +245,7 @@
         return;
       }
       $(".active-tag").removeClass("active active-tag");
-      $(this).addClass("active-tag");
+      $(this).addClass("active active-tag");
 
       var tag = $(this).data("images-toggle");
 
